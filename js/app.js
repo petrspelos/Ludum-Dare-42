4c225fb7d69e5a368c46a1a3d75addb29190bd42
Petr Sedláček <petr.spelos@gmail.com>
2018-08-11 15:15:38 +0200

    Basic libraries

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// function runPackage(icon, callback) {
+//     var tl = new TimelineLite();
+//     $box = $('<button class="btn btn-outline-secondary btn-item">' + icon + '</button>');
+//     $("body").append($box);
+//     $box.on("click", callback);
+//     tl.to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:randomWidth(),y:randomHeight(), ease: Elastic})
+//      .to($box, 1, {x:-100,y:-100,onComplete: removeJQueryElement, onCompleteParams: [$box]});
+//     tl.play();
+// }
+
+console.log("Hello, LD jam");
