556895d36fd3f81b12eb359c571c43887c60f5b7
Petr Sedláček <petr.spelos@gmail.com>
2018-08-12 19:37:16 +0200

    Installer delay

--- js/utility.js
+++ js/utility.js
@@ -72,6 +72,7 @@ function AppSetInstalled(appName, installed)
     IconsReload();
 }
 
+var InstallerTimeout; // Important for multiple installations.
 
 function AppsGetAllInstalled()
 {
@@ -84,6 +85,12 @@ function AppsGetAllInstalled()
             appList[appNa] = app;
     }
     return appList;
+}
+
+function CallAfterSeconds(callback, seconds)
+{
+    clearTimeout(InstallerTimeout);
+    InstallerTimeout = setTimeout(function() { callback(); }, seconds * 1000);
 }
 
 function SpaceAdd(amount, countForScore=false)
