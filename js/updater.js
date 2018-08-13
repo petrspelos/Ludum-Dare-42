f3d51812d65daeb9953b5deb340550e1e82ea8a4
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-08-13 16:55:28 +0200

    Add 5 achievements

--- js/updater.js
+++ js/updater.js
@@ -55,6 +55,8 @@ function RandomVirusInstallationCheck()
     }
 }
 
+StartButtonClickCount = 0;
+
 function ShakeStart()
 {
     $startBtn = $('#startBtn');
@@ -64,6 +66,16 @@ function ShakeStart()
     .to($startBtn, 0.1, {x: -10, ease: Elastic})
     .to($startBtn, 0.1, {x: 0, ease: Elastic});
     tl.play();
+
+    StartButtonClickCount++;
+    if (StartButtonClickCount === 5)
+    {
+        AchievementCompleteById('itDoesNothing');
+    }
+    else if (StartButtonClickCount === 50)
+    {
+        AchievementCompleteById('seriouslyPointless');
+    }
 }
 
 var progressPercentage = 10;
